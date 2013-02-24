4d62b6fd0c8aa3e98d278b9e72454b1828929e93
blackrim <blackrim@gmail.com>
2013-02-24 02:52:38 -0500

    consensus seqs

---
+++ src/main_conseq.cpp
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "fasta_util.h"
+#include "sequence.h"
+#include "seq_utils.h"
+
+int main(int argc, char * argv[]){
+    
+    if (argc != 3){
+	cout << "usage: pxconseq infile.fasta outfile.fasta" << endl;
+	exit(0);
+    }
+
+    vector<Sequence> seqs;
+    bool ret = read_fasta_file(argv[1],seqs);
+    if (ret == false){
+	cout << argv[1] << " is not a valid fasta file " << endl;
+	exit(0);
+    }
+    cout << seqs.size() << " sequences read" << endl;
+    string rets = consensus_seq(seqs,0);
+    ofstream ofs;
+    ofs.open(argv[2],ios::out);
+    ofs << ">" << argv[1] << endl;
+    ofs << rets << endl;
+    ofs.close();
+}
