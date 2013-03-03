10fa21523604e691e87ca58e168e3174a498818f
blackrim <blackrim@gmail.com>
2013-03-02 23:32:57 -0500

    more main fixing

--- src/main_conseq.cpp
+++ src/main_conseq.cpp
@@ -5,31 +5,110 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <string.h>
+#include <getopt.h>
 
 using namespace std;
 
 #include "seq_reader.h"
 #include "sequence.h"
 #include "seq_utils.h"
+#include "utils.h"
+
+void print_help(){
+    cout << "Consensus sequences from nexus,phylip, fastq to fasta." << endl;
+    cout << "Can read from stdin or file" << endl;
+    cout << endl;
+    cout << "Usage: pxconseq [OPTION]... [FILE]..."<<endl;
+    cout << endl; 
+    cout << " -s, --seqf=FILE     input sequence file, stdin otherwise"<<endl;
+    cout << " -o, --outf=FILE     output sequence file, stout otherwise"<<endl;
+    cout << "     --help          display this help and exit"<<endl;
+    cout << "     --version       display version and exit"<<endl;
+    cout << endl;
+    cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" <<endl;
+    cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>"<<endl;
+}
+
+string versionline("pxconseq 0.1\nCopyright (C) 2013 FePhyFoFum\nLiscence GPLv2\nwritten by Stephen A. Smith (blackrim)");
+
+static struct option const long_options[] =
+{
+    {"seqf", required_argument, NULL, 's'},
+    {"outf", required_argument, NULL, 'o'},
+    {"help", no_argument, NULL, 'h'},
+    {"version", no_argument, NULL, 'V'},
+    {NULL, 0, NULL, 0}
+};
 
 int main(int argc, char * argv[]){
-    
-    if (argc != 3){
-	cout << "usage: pxconseq infile.fasta outfile.fasta" << endl;
-	exit(0);
+    bool going = true;
+    bool fileset = false;
+    bool outfileset = false;
+    char * seqf;
+    char * outf;
+    while(going){
+        int oi = -1;
+        int c = getopt_long(argc,argv,"s:o:hV",long_options,&oi);
+        if (c == -1){
+            break;
+        }
+        switch(c){
+            case 's':
+                fileset = true;
+                seqf = strdup(optarg);
+                break;
+            case 'o':
+                outfileset = true;
+                outf = strdup(optarg);
+                break;
+            case 'h':
+                print_help();
+                exit(0);
+            case 'V':
+                cout << versionline << endl;
+                exit(0);
+            default:
+                print_error(argv[0],(char)c);
+                exit(0);
+        }
     }
-
+    istream* pios;
+    ostream* poos;
+    ifstream* fstr;
+    ofstream* ofstr; 
+    if(fileset == true){
+        fstr = new ifstream(seqf);
+        pios = fstr;
+    }else{
+        pios = &cin;
+    }
+    if(outfileset == true){
+        ofstr = new ofstream(seqf);
+        poos = ofstr;
+    }else{
+        poos = &cout;
+    }
+    Sequence seq;
+    string retstring;
     vector<Sequence> seqs;
-    bool ret = read_fasta_file(argv[1],seqs);
-    if (ret == false){
-	cout << argv[1] << " is not a valid fasta file " << endl;
-	exit(0);
+	int ft = test_seq_filetype_stream(*pios,retstring);
+	while(read_next_seq_from_stream(*pios,ft,retstring,seq)){
+	    seqs.push_back(seq);
     }
-    cout << seqs.size() << " sequences read" << endl;
+    if(ft == 2){
+        seqs.push_back(seq);
+    }
     string rets = consensus_seq(seqs,0);
-    ofstream ofs;
-    ofs.open(argv[2],ios::out);
-    ofs << ">" << argv[1] << endl;
-    ofs << rets << endl;
-    ofs.close();
+
+    (*poos) << ">consensus" << endl;
+    (*poos) << rets << endl;
+
+    if(fileset){
+        fstr->close();
+        delete pios;
+    }if(outfileset){
+        ofstr->close();
+        delete poos;
+    }
 }
