3e6d52d1328c3452f251643242152f779a4ce322
josephwb <phylo.jwb@gmail.com>
2013-03-05 00:07:01 -0500

    fix spacing in resampling code

--- src/seq_samp.h
+++ src/seq_samp.h
@@ -3,19 +3,19 @@
 
 class SequenceSampler {
 private:
-	float jkfract;
-	bool jackknife;
-	vector <int> samplesites;
-	
-	void get_bootstrap_sites (int const&);
-	void get_jackknife_sites (int const&);
+    float jkfract;
+    bool jackknife;
+    vector <int> samplesites;
+    
+    void get_bootstrap_sites (int const&);
+    void get_jackknife_sites (int const&);
 
 public:
-	SequenceSampler (int const&, float const&);
-	vector <int> get_sampled_sites ();
-	void sample_sites (int const&);
-	string get_resampled_seq (string const& origseq);
-	//~SequenceResampler();
+    SequenceSampler (int const&, float const&);
+    vector <int> get_sampled_sites ();
+    void sample_sites (int const&);
+    string get_resampled_seq (string const& origseq);
+    //~SequenceResampler();
 };
 
 #endif /* SEQ_SAMP_H_ */
