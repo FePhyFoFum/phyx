8ed487e4f7efde52089469569f18384ace0ef0a4
blackrim <blackrim@gmail.com>
2013-03-04 22:43:55 -0500

    nw align

---
+++ src/pairwise_alignment.h
@@ -0,0 +1,15 @@
+#ifndef PAIRWISE_ALIGNMENT_H
+#define PARIWISE_ALIGNMENT_H
+
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+#include "sequence.h"
+
+double nw(Sequence & seq1, Sequence & seq2, map<char, map<char,int> > & sc_mat, double gap_penalty, string & aln1, string & aln2);
+
+#endif
--- src/seq_models.h
+++ src/seq_models.h
@@ -4,11 +4,12 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <string>
 
 using namespace std;
 
 #include "sequence.h"
 
-void read_scoring_matrix(filename, map<char, map<char,int> > & sc_mat);
+void read_scoring_matrix(char * filename, map<char, map<char,int> > & sc_mat);
 
 #endif
