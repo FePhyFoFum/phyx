ece5801c1ea87353f88f5749b6a81a7049223970
Joseph W. Brown <phylo.jwb@gmail.com>
2015-01-27 17:06:25 -0500

    concat

--- src/concat.cpp
+++ src/concat.cpp
@@ -1,269 +1,138 @@
-/*
- * concat.cpp
- *
- *  Created on: Sep 22, 2014
- *      Author: joe
- */
-
-
-
-#include <iostream>
 #include <string>
-#include <fstream>
 #include <vector>
+#include <iostream>
 #include <sstream>
-#include <iterator>
-//#include <string.h>
+#include <cmath>
+#include <limits>
+#include <cstdlib>
 #include <algorithm>
-//#include <set>
+#include <ctime>
+#include <fstream>
+
 using namespace std;
 
-//Gets the length of the string
-int getlength(string seq){
+#include "utils.h"
+#include "sequence.h"
+#include "seq_reader.h"
+#include "concat.h"
 
-	int seq_length;
-	seq_length = seq.size();
-	return seq_length;
-
-}
-//function splits files on tab and returns vector
-//each element of the vector contains what has been
-//split on the tab, so element 0 is name and
-//element 1 is the sequence itself
-vector<string> tab_split(string dna){
-
-	string second_dna_file;
-	int count = 0;
-	int Length_of_token;
-	string line;
-	ifstream input_dna;
-	vector<string> names; //vector to contain names
-	vector<string> sequences; //vector to contain sequences
-	string token;
-	input_dna.open(dna.c_str());
-		if (input_dna.is_open()){
-
-			//split into separate strings
-			while (getline (input_dna, line)){
-				//cout << line << endl;
-				istringstream str(line);
-				if (count != 0){
-					while(getline(str, token, ' ')){
-						//create a vector with name and sequence as elements
-						 //   token.erase(std::remove(token.begin(), token.end(), ' '), token.end());
-							Length_of_token = getlength(token);
-							if (Length_of_token != 0){
-								cout << "After Erase" << token << '\n';
-								names.push_back(token);
-						//	cout << token << endl;
-							}
-					}
-				}
-				count++;
-			}
-		}
-		return names;
+SequenceConcatenater::SequenceConcatenater (string & seqf):numPartitions(0), numChar(0), numTaxa(0), ft(0) {
+    read_sequences(seqf);
 }
 
-//not a good way but the best I can come up with now
-//to return all the unique values of a vector
-vector<string> matcher(vector<string>& unique_ones, vector<string>& match)
-{
-	//sorts uniquely and removes duplicates
-	 vector<string>::iterator it;
-	 vector<string> no_dup;
-     sort(unique_ones.begin(), unique_ones.end());
-     it = unique(unique_ones.begin(), unique_ones.end());
-     unique_ones.resize(distance(unique_ones.begin(),it));
-     unique_ones.push_back("@!#!@$!@$!@$!@");
-
-     for(vector<string>::size_type i = 0; i != match.size(); i++) {
-			for(vector<string>::size_type j = 0; j != unique_ones.size(); j++){
-				if(unique_ones[j] == match[i]){
-					unique_ones.erase(remove(unique_ones.begin(), unique_ones.end(), unique_ones[j]), unique_ones.end());
-				}
-
-			}
-		}
-     return unique_ones;
-}
-
-//where the strings get concatenated
-//not great but doesn't suck nearly bad as attempts 1 or 2
-//vector inputs are the running string called sequences and
-//the new string called second_sequences
-//integer inputs are the running length of a string and
-//the length of the newest string
-vector<string> concat(vector<string>& old_vec, vector<string>& new_vec, int current_size, int running_size){
-
-	vector<string> phylip;
-	vector<string> no_match_old;
-	vector<string> no_match_new;
-	vector<string> match;
-	vector<string> unique_old;
-	vector<string> unique_new;
-	int old_match_count = 0;
-	int new_match_count = 0;
-	string cat;
-	for(vector<string>::size_type i = 0; i != old_vec.size(); i = i + 2) {
-
-		cout << "old vector: " << old_vec[i] << "\t" << old_vec[i + 1] << '\n';
-	}
-	for(vector<string>::size_type i = 0; i != new_vec.size(); i = i + 2) {
-
-		cout << "new vector: " << new_vec[i] << "\t" << new_vec[i + 1] << '\n';
-	}
-
-	//find matches of old vector to new
-	for(vector<string>::size_type i = 0; i != old_vec.size(); i = i + 2) {
-			for(vector<string>::size_type j = 0; j != new_vec.size(); j = j + 2){
-				if (old_vec[i] == new_vec[j]){
-					cat = old_vec[i + 1] + new_vec[j+1];
-					phylip.push_back(old_vec[i]);
-					phylip.push_back(cat);
-					match.push_back(old_vec[i]);
-
-				}else{
-					//filled to the top with non_matches
-					no_match_old.push_back(old_vec[i]);
-					no_match_new.push_back(new_vec[j]);
-				}
-			}
-	}
-	for(vector<string>::size_type i = 0; i != old_vec.size(); i = i + 2) {
-		for(vector<string>::size_type j = 0; j != match.size(); j++){
-			if (match[j] == old_vec[i]){
-
-				old_match_count++;
-			}
-		}
-
-	}
-	for(vector<string>::size_type i = 0; i != new_vec.size(); i = i + 2) {
-		for(vector<string>::size_type j = 0; j != match.size(); j++){
-			if (match[j] == new_vec[i]){
-
-				new_match_count++;
-			}
-		}
-
-	}
-	if ((old_vec.size() / 2) != old_match_count){
-		unique_old = matcher(no_match_old, match);
-		for(vector<string>::size_type i = 0; i != unique_old.size(); i++) {
-			cout << "Unique old ones: " << unique_old[i] << '\n';
-		}
-		for(vector<string>::size_type i = 0; i != unique_old.size(); i++) {
-			for(vector<string>::size_type j = 0; j != old_vec.size(); j = j + 2){
-				if (unique_old[i] == old_vec[j]){
-					string stuff(current_size, 'N');
-					cat = old_vec[j + 1] + stuff;
-					phylip.push_back(old_vec[j]);
-					phylip.push_back(cat);
-
-				}
-			}
-		}
-	}
-	if ((new_vec.size() / 2) != new_match_count){
-		unique_new = matcher(no_match_new, match);
-		for(vector<string>::size_type i = 0; i != unique_new.size(); i++) {
-			for(vector<string>::size_type j = 0; j != new_vec.size(); j = j + 2){
-				if(unique_new[i] == new_vec[j]){
-
-					string stuff(running_size, 'N');
-					cat = stuff + new_vec[j + 1];
-					phylip.push_back(new_vec[j]);
-					phylip.push_back(cat);
-
-				}
-
-			}
-		}
-	}
-	return phylip;
-}
-int main()
-{
-
-	string dna;
-	string second_dna_file;
-	string line;
-	string cat;
-	char cont = 'Y';
-	int while_count = 0;
-	int running_size = 0;
-	int current_size = 0;
-	int total_size = 0;
-	int count = 0;
-	vector<string> together;
-	ifstream input_dna;
-	ofstream concatenated;
-	ofstream final_product;
-	concatenated.open("sequence3.txt");
-	final_product.open("final.phy");
-	vector<string> temporary; //vector to contain names
-	vector<string> sequences; //vector to contain sequences
-	vector<string> second_sequences;
-	string token;
-	//Here is where the file is read in line by line
-	cout << "enter dna: ";
-	cin >> dna;
-	//have new sequences enter here
-
-	while(cont == 'Y'){
-
-		cout << "enter next: ";
-		cin >> second_dna_file;
-		cout << "continue: ";
-		cin >> cont;
-		if(while_count == 0){
-
-			sequences = tab_split(dna);
-			running_size = getlength(sequences[1]);
-			concatenated << "The size is: " << running_size << endl;
-			second_sequences = tab_split(second_dna_file);
-			current_size = getlength(second_sequences[1]);
-			concatenated << "The size of new sequence is: " << current_size << endl;
-			together = concat(sequences, second_sequences, current_size, running_size);
-			while_count++;
-			total_size = current_size + running_size;
-			concatenated << "The running size is: " << total_size << endl;
-
-		}else{
-			second_sequences = tab_split(second_dna_file);
-			current_size = getlength(second_sequences[1]);
-			together = concat(together, second_sequences, current_size, total_size);
-			total_size = current_size + total_size;
-			concatenated << "The size is: " << current_size << endl;
-			concatenated << "The running size is: " << total_size << endl;
-		}
-		for(vector<string>::size_type i = 0; i != together.size(); i++){
-
-			cout << together[i] << "\n";
-		}
-
-	}
-	//Here is where the phylip file is printed out
-	final_product << (together.size() / 2) << " " << total_size << "\n";
-	for(vector<string>::size_type i = 0; i != together.size(); i++){
-
-		if (count == 1){
-			final_product << together[i] << "\n";
-			count = 0;
-		}else{
-			final_product << together[i] << "\t";
-			count = 1;
-		}
-	}
-	//Here is where they are closed
-	final_product.close();
-	concatenated.close();
-	return 0;
+SequenceConcatenater::SequenceConcatenater ():numPartitions(0), numChar(0), numTaxa(0), ft(0) {
 
 }
 
+void SequenceConcatenater::read_sequences (string & seqf) {
+    filename = seqf;
+    string retstring;
+    istream* pios = new ifstream(filename);
+    ft = test_seq_filetype_stream(*pios, retstring);
+    Sequence seq;
+    int counter = 0;
+    int length = 0;
 
+    cout << "ft = " << ft << "; retstring = " << retstring << endl;
+    if (ft == 1) { // phylip
+        vector <string> fileDim;
+        tokenize(retstring, fileDim, " ");
+        numTaxa = stoi(fileDim[0]);
+        numChar = stoi(fileDim[1]);
 
+        //cout << "numTaxa = " << numTaxa << "; numChar = " << numChar << "." << endl;
 
+        while (read_next_seq_from_stream(*pios, ft, retstring, seq)) {
+            length = (int)seq.get_sequence().size();
+            if (length != numChar) {
+                cout << "Sequence '" << seq.get_id() << "' has " << length << " characters, but the file '"
+                    << filename << "' specified " << numChar << "characters. Exiting." << endl;
+                delete pios;
+                exit(1);
+            }
+            seqs.push_back(seq);
+            counter++;
+        }
+        if (counter != numTaxa) {
+            cout << "Read " << counter << " taxa, but the file '" << filename << "' specified "
+                << numTaxa << "taxa. Exiting." << endl;
+            delete pios;
+            exit(1);
+        }
+    } else { // will need error checking for other file types
+        bool first = true;
+        while (read_next_seq_from_stream(*pios, ft, retstring, seq)) {
+            int curr = (int)seq.get_sequence().size();
+            if (!first) {
+                if (curr != length) {
+                    cout << "Error: current sequence has " << curr << " characters, but previous sequence had "
+                        << length << " characters. Exiting." << endl;
+                    delete pios;
+                    exit(1);
+                }
+            } else {
+                length = curr;
+                first = false;
+            }
+            seqs.push_back(seq);
+            cout << "Taxon: " << seq.get_id() << endl;
+            counter++;
+        }
+        numTaxa = counter;
+        numChar = length;
+    }
+    numPartitions = 1;
+    partitionSizes.push_back(numChar);
+    delete pios;
+}
 
+// where stuff actually happens
+void SequenceConcatenater::concatenate(SequenceConcatenater & newSeqs) {
+    string old_filler(numChar, 'N');
+    string new_filler(newSeqs.get_sequence_length(), 'N');
+
+    for (int i = 0; i != numTaxa; i++) {
+        bool match_found = false;
+        if (newSeqs.numTaxa > 0) {
+            for (int j = 0; j != newSeqs.numTaxa; j++) {
+                if (seqs[i].get_id() == newSeqs.seqs[j].get_id()) {
+                    seqs[i].set_sequence(seqs[i].get_sequence() + newSeqs.seqs[j].get_sequence());
+                    match_found = true;
+                    // erase matched entry so it won't have to be compared against again.
+                    // erase in reverse order.
+                    delete_sequence(newSeqs, j);
+                    break;
+                }
+            }
+        }
+        if (!match_found) { // taxon is missing from present locus.
+            seqs[i].set_sequence(seqs[i].get_sequence() + new_filler);
+        }
+    }
+
+    // now, all that should be left are the novel sequences from the new file
+    if (newSeqs.numTaxa > 0) {
+        for (int i = 0; i != newSeqs.numTaxa; i++) {
+            newSeqs.seqs[i].set_sequence(old_filler + newSeqs.seqs[i].get_sequence());
+            seqs.push_back(newSeqs.seqs[i]);
+            numTaxa++;
+        }
+    }
+}
+
+int SequenceConcatenater::get_sequence_length () {
+    return numChar;
+}
+
+int SequenceConcatenater::get_num_taxa () {
+    return numTaxa;
+}
+
+void SequenceConcatenater::delete_sequence (SequenceConcatenater & newSeqs, int const& index) {
+    newSeqs.seqs.erase(newSeqs.seqs.begin() + index);
+    newSeqs.numTaxa--;
+}
+
+Sequence SequenceConcatenater::get_sequence (int const & index) {
+    return seqs[index];
+}
--- src/utils.cpp
+++ src/utils.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <stdlib.h>
 #include <math.h>
+#include <string>
 #include "utils.h"
 #include "superdouble.h"
 
@@ -29,17 +30,27 @@ void tokenize(const string& str, vector<string>& tokens,
     }
 }
 
+// works a little differently than above; don't need to trim spaces
+vector <string> tokenize (string const& input) {
+    vector <string> tokens;
+    string temp;
+    istringstream str(input);
+    while (str >> temp) {
+        tokens.push_back(temp);
+    }
+    return tokens;
+}
+
 void trim_spaces(string& str) {
     // Trim Both leading and trailing spaces
     size_t startpos = str.find_first_not_of(" \t\r\n"); // Find the first character position after excluding leading blank spaces
     size_t endpos = str.find_last_not_of(" \t\r\n"); // Find the first character position from reverse af
 
     // if all spaces or empty return an empty string
-    if(( string::npos == startpos ) || ( string::npos == endpos)){
-	str = "";
-    }
-    else{
-	str = str.substr( startpos, endpos-startpos+1 );
+    if (( string::npos == startpos ) || ( string::npos == endpos)) {
+	    str = "";
+    } else {
+	    str = str.substr( startpos, endpos-startpos+1 );
     }
     /*
     // Code for  Trim Leading Spaces only
@@ -59,7 +70,7 @@ void trim_spaces(string& str) {
 bool is_number(const string& s){
     string::const_iterator it = s.begin();
     while (it != s.end() && std::isdigit(*it)){ 
-	++it;
+	    ++it;
     }
     return !s.empty() && it == s.end();
 }
@@ -96,23 +107,23 @@ vector<vector<double> > processRateMatrixConfigFile(string filename, int numstat
     string line;
     int fromarea = 0;
     while(getline(ifs,line)){
-	if(line.size() > 3){
-	    vector<string> tokens;
-	    string del(" ,\t");
-	    tokens.clear();
-	    tokenize(line, tokens, del);
-	    for(unsigned int j=0;j<tokens.size();j++){
-		trim_spaces(tokens[j]);
-	    }
-	    for(unsigned int j=0;j<tokens.size();j++){
-		ratematrix[fromarea][j] = atof(tokens[j].c_str());
-	    }
-	    if(fromarea < numstates-1){
-		fromarea += 1;
-	    }else{
-		fromarea = 0;
-	    }
-	}
+        if(line.size() > 3){
+            vector<string> tokens;
+            string del(" ,\t");
+            tokens.clear();
+            tokenize(line, tokens, del);
+            for(unsigned int j=0;j<tokens.size();j++){
+                trim_spaces(tokens[j]);
+            }
+            for(unsigned int j=0;j<tokens.size();j++){
+                ratematrix[fromarea][j] = atof(tokens[j].c_str());
+            }
+            if(fromarea < numstates-1){
+                fromarea += 1;
+            }else{
+                fromarea = 0;
+            }
+        }
     }
     ifs.close();
     return ratematrix;
@@ -142,7 +153,7 @@ void print_error(char * pname, char arg){
 int sum_matrix_col(vector<vector<int> > & matrix,int col){
     int x=0;
     for(unsigned int i=0;i<matrix.size();i++){
-	x += matrix[i][col];
+	    x += matrix[i][col];
     }
     return x;
 }
@@ -150,8 +161,9 @@ int sum_matrix_col(vector<vector<int> > & matrix,int col){
 int sum_matrix_col_negs(vector<vector<int> > & matrix,int col){
     int x=0;
     for(unsigned int i=0;i<matrix.size();i++){
-	if(matrix[i][col] < 0)
-	    x += matrix[i][col];
+        if(matrix[i][col] < 0) {
+            x += matrix[i][col];
+        }
     }
     return x;
 }
@@ -161,18 +173,18 @@ bool test_logical(vector<int> & matA,vector<int> & matB){
     int match1 = 0;
     unsigned int numdiffs = 0;
     for (unsigned int i=0;i<matA.size();i++){
-	//added the -1, can take out if something gets fishy
-	if (((matA[i] == 1) && (matB[i] == 1))){
-	    match1 += 1;
-	}else{
-	    numdiffs += 1;
-	}
+        //added the -1, can take out if something gets fishy
+        if (((matA[i] == 1) && (matB[i] == 1))){
+            match1 += 1;
+        }else{
+            numdiffs += 1;
+        }
     }
     //had to change because of negatives
     if ((match1 != sum(matA)) && (match1 != sum(matB))){    
-	if (numdiffs != matA.size()){
-	    test = true;
-	}
+        if (numdiffs != matA.size()){
+            test = true;
+        }
     }
     
     return test;
@@ -182,7 +194,7 @@ bool test_logical(vector<int> & matA,vector<int> & matB){
 double sum(vector<double> &inm){
     double x=0;
     for(unsigned int i=0;i<inm.size();i++){
-	x += inm[i];
+	    x += inm[i];
     }
     return x;
 }
@@ -190,7 +202,7 @@ double sum(vector<double> &inm){
 double sum(vector<int> &inm){
     double x=0;
     for(unsigned int i=0;i<inm.size();i++){
-	x += inm[i];
+	    x += inm[i];
     }
     return x;
 }
@@ -198,7 +210,7 @@ double sum(vector<int> &inm){
 string get_string_vector(vector<string> &sts){
     string rets;
     for(int i=0;i<sts.size();i++){
-	rets += sts[i]+ " ";
+	    rets += sts[i]+ " ";
     }
     return rets;
 }
@@ -206,7 +218,7 @@ string get_string_vector(vector<string> &sts){
 string get_string_vector(vector<int> & sts){
     string rets;
     for(int i=0;i<sts.size();i++){
-	rets += to_string(sts[i]) + " ";
+	    rets += to_string(sts[i]) + " ";
     }
     return rets;
 }
