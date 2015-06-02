c25e0ac31607af4dd61e6626ebe12c9dcbe16e42
jfwalker <jfwalker@umich.edu>
2015-06-02 13:25:16 -0400

    Added Simple Translate Program

---
+++ src/pxtrlate.h
@@ -0,0 +1,17 @@
+/*
+ * pxtrlate.h
+ *
+ *  Created on: Jun 2, 2015
+ *      Author: joe
+ */
+
+#ifndef PXTRLATE_H_
+#define PXTRLATE_H_
+
+class pxtrlate {
+public:
+	pxtrlate();
+	virtual ~pxtrlate();
+};
+
+#endif /* PXTRLATE_H_ */
