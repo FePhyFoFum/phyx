a674245fbc89b76baa370d68886b085c6d45cc29
Joseph W. Brown <phylo.jwb@gmail.com>
2015-06-10 15:02:49 -0400

    fix pxmrcacut

--- src/main_mrca_cut.cpp
+++ src/main_mrca_cut.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cstring>
 #include <getopt.h>
 
 using namespace std;
@@ -19,62 +20,155 @@ using namespace std;
 #include "vector_node_object.h"
 #include "tree.h"
 #include "utils.h"
-#include "bd_sim.h"
+#include "tree_utils.h"
 
+void print_help(){
+    cout << "Extract subclade(s) from tree(s)." << endl;
+    cout << "Takes in newick tree and MRCA file with format:" << endl;
+    cout << "MRCANAME = tip1 tip2 ..." << endl;
+    cout << endl;
+    cout << "Usage: pxmrcacut [OPTION]... " << endl;
+    cout << endl;
+    cout << " -t, --treef=FILE    input newick tree file, stdin otherwise" << endl;
+    cout << " -o, --outf=FILE     output newick file, stout otherwise" << endl;
+    cout << " -m, --mrca=FILE     file containing MRCA declarations" << endl;
+    cout << "     --help          display this help and exit" << endl;
+    cout << "     --version       display version and exit" << endl;
+    cout << endl;
+    cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" << endl;
+    cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>" << endl;
+}
+
+string versionline("pxmrcacut 0.1\nCopyright (C) 2013 FePhyFoFum\nLicense GPLv2\nwritten by Stephen A. Smith (blackrim), Joseph W. Brown");
+
+static struct option const long_options[] =
+{
+    {"treef", required_argument, NULL, 't'},
+    {"outf", required_argument, NULL, 'o'},
+    {"mrca", required_argument, NULL, 'm'},
+    {"help", no_argument, NULL, 'h'},
+    {"version", no_argument, NULL, 'V'},
+    {NULL, 0, NULL, 0}
+};
 
 int main(int argc, char * argv[]){
     TreeReader tr;
+    char * outf;
+    char * treef;
+    char * mrcaf;
+    bool outfileset = false;
+    bool fileset = false;
+    bool mrcaset = false;
+    
+    while (1) {
+        int oi = -1;
+        int c = getopt_long(argc, argv, "t:o:m:hV", long_options, &oi);
+        if (c == -1) {
+            break;
+        }
+        switch(c) {
+            case 't':
+                fileset = true;
+                treef = strdup(optarg);
+                break;
+            case 'o':
+                outfileset = true;
+                outf = strdup(optarg);
+                break;
+            case 'm':
+                mrcaset = true;
+                mrcaf = strdup(optarg);
+                break;
+            case 'h':
+                print_help();
+                exit(0);
+            case 'V':
+                cout << versionline << endl;
+                exit(0);
+            default:
+                print_error(argv[0],(char)c);
+                exit(0);
+        }
+    }
 
-    if (argc != 3){
-        cout << "usage: phyx_mrca_cut MRCA newickfile" << endl;
+    if (!mrcaset) {
+        cout << "Must supply mrca file." << endl;
         exit(0);
     }
-
-    ifstream infile(argv[1]);
-    if (!infile){
-        cerr << "Could not open mrcafile." << endl;
-        return 1;
+    
+    istream* pios;
+    ostream* poos;
+    ifstream* fstr;
+    ofstream* ofstr;
+    
+    if (outfileset == true) {
+        ofstr = new ofstream(outf, ios::app);
+        poos = ofstr;
+    } else {
+        poos = &cout;
     }
-
+    
+    if (fileset == true) {
+        fstr = new ifstream(treef);
+        pios = fstr;
+    } else {
+        pios = &cin;
+    }
+    
+    /* 
+       collect clade names
+       expecting (new) format:
+       MRCANAME = tip1 tip2 ... 
+    */
+    ifstream inmrca(mrcaf);
     string mrcaline;
-    map<string,vector<string> > mrcas;
-    while (getline(infile, mrcaline)){
+    map<string, vector<string> > mrcas;
+    while (getline(inmrca, mrcaline)){
+        if (mrcaline.empty()) {
+            continue;
+        }
         vector<string> searchtokens;
-        tokenize(mrcaline, searchtokens, "     ");
-            for(unsigned int j=0;j<searchtokens.size();j++){
-                trim_spaces(searchtokens[j]);
-            }
-        vector<string> vec;vec.push_back(searchtokens[1]);vec.push_back(searchtokens[2]);
-        mrcas[searchtokens[0]] = vec;
+        tokenize(mrcaline, searchtokens, "=");
+        string mrcaname = searchtokens[0];
+        trim_spaces(mrcaname);
+        searchtokens.erase(searchtokens.begin());
+        searchtokens = tokenize(searchtokens[0]);
+        mrcas[mrcaname] = searchtokens;
     }
-    infile.close();
-
-
-
-    ifstream infile2(argv[2]);
-    if (!infile2){
-        cerr << "Could not open treefile." << endl;
-        return 1;
-    }
+    inmrca.close();
+    
+    // collect tree(s)
     vector<string> lines;
     string line;
-    while (getline(infile2, line)){
+    while (getline(*pios, line)){
         lines.push_back(line);
     }
-    infile2.close();
+    
+    for (int i = 0; i < (int)lines.size(); i++) {
+        Tree * tree = tr.readTree(lines[i]);
+        cout << tree->getExternalNodeCount() << endl;
 
-    Tree * tree = tr.readTree(lines[0]);
-    cout << tree->getExternalNodeCount() << endl;
-
-    map<string,vector<string> >::iterator it;
-    for (it=mrcas.begin(); it!=mrcas.end(); it++){
-        Node * nd = tree->getMRCA((*it).second);
-        ofstream outFile;
-        outFile.open((*it).first.c_str(), ios::out);
-        outFile << nd->getNewick(true) << endl;
-        outFile.close();
+        map<string,vector<string> >::iterator it;
+        for (it = mrcas.begin(); it != mrcas.end(); it++){
+            //cout << "Dealing with clade '" << (*it).first << "'" << endl;
+            if (!check_names_against_tree(tree, (*it).second)) {
+                cout << "Check mrca file for typos." << endl;
+                exit (0);
+            }
+            Node * nd = tree->getMRCA((*it).second);
+            (*poos) << nd->getNewick(true) << ";" << endl;
+        }
+        delete tree;
     }
-
-    delete tree;
+    
+    if (fileset) {
+        fstr->close();
+        delete pios;
+    }
+    if (outfileset) {
+        ofstr->close();
+        delete poos;
+    }
+    
     return EXIT_SUCCESS;
 }
