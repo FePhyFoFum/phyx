7f2c16591acdfbeb52fcd5f84c397964bdae58c2
Joseph W. Brown <phylo.jwb@gmail.com>
2016-05-11 12:57:01 -0400

    update pxmrca with help, getopt

--- src/main_mrca.cpp
+++ src/main_mrca.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cstring>
 #include <getopt.h>
 
 using namespace std;
@@ -19,87 +20,144 @@ using namespace std;
 #include "utils.h"
 #include "bd_sim.h"
 
-/*
 void print_help(){
-    cout << "Get information about an mrca" << endl;
+    cout << "Get information about an mrca." << endl;
+    cout << "Takes in newick tree and MRCA file with format:" << endl;
+    cout << "MRCANAME = tip1 tip2 ..." << endl;
     cout << endl;
-    cout << "Usage: pxmrca [OPTION]... [FILE]..."<<endl;
-    cout << endl; 
-    cout << " -m, --mean=VALUE    mean value under which seqs are filtered" << endl;
-    cout << " -s, --seqf=FILE     input sequence file, stdin otherwise" << endl;
-    cout << " -o, --outf=FILE     output sequence file, stout otherwise" << endl;
+    cout << "Usage: pxmrcacut [OPTION]... " << endl;
+    cout << endl;
+    cout << " -t, --treef=FILE    input newick tree file, stdin otherwise" << endl;
+    cout << " -o, --outf=FILE     output newick file, stout otherwise" << endl;
+    cout << " -m, --mrca=FILE     file containing MRCA declarations" << endl;
     cout << "     --help          display this help and exit" << endl;
     cout << "     --version       display version and exit" << endl;
     cout << endl;
-    cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" <<endl;
-    cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>"<<endl;
+    cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" << endl;
+    cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>" << endl;
 }
-
 string versionline("pxmrca 0.1\nCopyright (C) 2013 FePhyFoFum\nLiscence GPLv2\nwritten by Stephen A. Smith (blackrim)");
 
 static struct option const long_options[] =
 {
-    {"mean", required_argument, NULL, 'm'},
-    {"seqf", required_argument, NULL, 's'},
+    {"treef", required_argument, NULL, 't'},
     {"outf", required_argument, NULL, 'o'},
+    {"mrca", required_argument, NULL, 'm'},
     {"help", no_argument, NULL, 'h'},
     {"version", no_argument, NULL, 'V'},
     {NULL, 0, NULL, 0}
 };
 
-*/
-
 int main(int argc, char * argv[]) {
     TreeReader tr;
     
-    if (argc != 3) {
-        cout << "usage: pxmrca MRCA newickfile" << endl;
+    char * outf;
+    char * treef;
+    char * mrcaf;
+    bool outfileset = false;
+    bool fileset = false;
+    bool mrcaset = false;
+    
+    while (1) {
+        int oi = -1;
+        int c = getopt_long(argc, argv, "t:o:m:hV", long_options, &oi);
+        if (c == -1) {
+            break;
+        }
+        switch(c) {
+            case 't':
+                fileset = true;
+                treef = strdup(optarg);
+                break;
+            case 'o':
+                outfileset = true;
+                outf = strdup(optarg);
+                break;
+            case 'm':
+                mrcaset = true;
+                mrcaf = strdup(optarg);
+                break;
+            case 'h':
+                print_help();
+                exit(0);
+            case 'V':
+                cout << versionline << endl;
+                exit(0);
+            default:
+                print_error(argv[0],(char)c);
+                exit(0);
+        }
+    }
+    
+    if (!mrcaset) {
+        cout << "Must supply mrca file." << endl;
         exit(0);
     }
     
-    ifstream infile(argv[1]);
-    if (!infile) {
-        cerr << "Could not open mrcafile." << endl;
-        return 1;
+    istream* pios;
+    ostream* poos;
+    ifstream* fstr;
+    ofstream* ofstr;
+    
+    if (outfileset == true) {
+        ofstr = new ofstream(outf, ios::app);
+        poos = ofstr;
+    } else {
+        poos = &cout;
     }
     
+    if (fileset == true) {
+        fstr = new ifstream(treef);
+        pios = fstr;
+    } else {
+        pios = &cin;
+    }
+    
+    ifstream inmrca(mrcaf);
     string mrcaline;
     map<string, vector<string> > mrcas;
-    while (getline(infile, mrcaline)){
-        vector<string> searchtokens;
-        tokenize(mrcaline, searchtokens, "     ");
-        cout << "Read in " << searchtokens.size() << " tokens!" << endl;
-        for (unsigned int j=0; j < searchtokens.size(); j++) {
-            trim_spaces(searchtokens[j]);
+    while (getline(inmrca, mrcaline)) {
+        if (mrcaline.empty()) {
+            continue;
         }
-        vector<string> vec;
-        vec.push_back(searchtokens[1]);
-        vec.push_back(searchtokens[2]);
-        mrcas[searchtokens[0]] = vec;
+        vector<string> searchtokens;
+        tokenize(mrcaline, searchtokens, "=");
+        string mrcaname = searchtokens[0];
+        trim_spaces(mrcaname);
+        searchtokens.erase(searchtokens.begin());
+        searchtokens = tokenize(searchtokens[0]);
+        mrcas[mrcaname] = searchtokens;
     }
-    infile.close();
+    inmrca.close();
     
-    ifstream infile2(argv[2]);
-    if (!infile2){
-        cerr << "Could not open treefile." << endl;
-        return 1;
-    }
+    // collect tree(s)
     vector<string> lines;
     string line;
-    while (getline(infile2, line)) {
+    while (getline(*pios, line)) {
         lines.push_back(line);
     }
-    infile2.close();
-
-    Tree * tree = tr.readTree(lines[0]);
-    cout << tree->getExternalNodeCount() << endl;
-
-    map<string,vector<string> >::iterator it;
-    for (it=mrcas.begin(); it != mrcas.end(); it++) {
-        Node * nd = tree->getMRCA((*it).second);
-        cout << (*it).first<<" "<< nd->get_num_leaves() << " " << nd->getName() << endl;
+    
+    for (unsigned int i = 0; i < lines.size(); i++) {
+        Tree * tree = tr.readTree(lines[i]);
+        cout << tree->getExternalNodeCount() << endl;
+        
+        map<string,vector<string> >::iterator it;
+        for (it=mrcas.begin(); it != mrcas.end(); it++) {
+            Node * nd = tree->getMRCA((*it).second);
+            cout << (*it).first << " " << nd->get_num_leaves() << " "
+                << nd->getName() << endl;
+        }
+        delete tree;
     }
-
-    delete tree;
+    
+    if (fileset) {
+        fstr->close();
+        delete pios;
+    }
+    if (outfileset) {
+        ofstr->close();
+        delete poos;
+    }
+    
     return EXIT_SUCCESS;
 }
