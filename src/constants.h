a9307fff04ccc8fea56a8a3011c19123bd4e20ed
Joseph W. Brown <phylo.jwb@gmail.com>
2016-05-24 12:54:13 -0400

    file to store numerical constants

---
+++ src/constants.h
@@ -0,0 +1,9 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+// a place to put global constants so they do not have to be redefined
+
+const long double PI = 3.1415926535897932384626433832795L;
+const long double E = 2.718281828459045;
+
+#endif /* _CONSTANTS_H_ */
