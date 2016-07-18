e42e092e4b10596bb0375a945a9e5c6d07f0a97f
Joseph W. Brown <phylo.jwb@gmail.com>
2016-07-18 10:57:02 -0400

    separate log code

---
+++ src/log.cpp
@@ -0,0 +1,20 @@
+// log program calls. useful for documenting and replication
+
+#include <fstream>
+
+using namespace std;
+
+#include "log.h"
+
+void log_call (int argc, char * argv[]) {
+    ofstream phyxlog;
+    phyxlog.open ("phyx.log", ios::out | ios::app);
+    for (unsigned i = 0; i < argc; i++) {
+        phyxlog << argv[i];
+        if (i < (argc - 1)) {
+            phyxlog << " ";
+        }
+    }
+    phyxlog << endl;
+    phyxlog.close();
+}
---
+++ src/log.h
@@ -0,0 +1,6 @@
+#ifndef _PHYXLOG_H_
+#define _PHYXLOG_H_
+
+void log_call (int argc, char * argv[]);
+
+#endif /* _PHYXLOG_H_ */
