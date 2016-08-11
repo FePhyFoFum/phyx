c346343690dc69d368b4d5417a5d447a03ffaba9
Joseph W. Brown <phylo.jwb@gmail.com>
2016-08-11 19:01:14 -0400

    starting vcftofa

---
+++ src/main_vcftofa.cpp
@@ -0,0 +1,107 @@
+// see specifications here: https://samtools.github.io/hts-specs/VCFv4.2.pdf
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <getopt.h>
+
+using namespace std;
+
+#include "utils.h"
+#include "vcf_reader.h"
+#include "log.h"
+
+void print_help() {
+    cout << "Convert vcf file to fasta." << endl;
+    cout << "Can read from stdin or file." << endl;
+    cout << endl;
+    cout << "Usage: pxvcftofa [OPTION]... [FILE]..." << endl;
+    cout << endl;
+    cout << " -s, --seqf=FILE     input vcf file, stdin otherwise" << endl;
+    cout << " -o, --outf=FILE     output fastasequence file, stout otherwise" << endl;
+    cout << " -h, --help          display this help and exit" << endl;
+    cout << " -V, --version       display version and exit" << endl;
+    cout << endl;
+    cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" << endl;
+    cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>" << endl;
+}
+
+string versionline("pxvcftofa 0.1\nCopyright (C) 2013 FePhyFoFum\nLicense GPLv3\nwritten by Joseph W. Brown, Stephen A. Smith (blackrim)");
+
+static struct option const long_options[] =
+{
+    {"seqf", required_argument, NULL, 's'},
+    {"outf", required_argument, NULL, 'o'},
+    {"help", no_argument, NULL, 'h'},
+    {"version", no_argument, NULL, 'V'},
+    {NULL, 0, NULL, 0}
+};
+
+int main(int argc, char * argv[]) {
+    
+    log_call(argc, argv);
+    
+    bool fileset = false;
+    bool outfileset = false;
+    char * seqf = NULL;
+    char * outf = NULL;
+    while (1) {
+        int oi = -1;
+        int c = getopt_long(argc, argv, "s:o:hV", long_options, &oi);
+        if (c == -1) {
+            break;
+        }
+        switch(c) {
+            case 's':
+                fileset = true;
+                seqf = strdup(optarg);
+                check_file_exists(seqf);
+                break;
+            case 'o':
+                outfileset = true;
+                outf = strdup(optarg);
+                break;
+            case 'h':
+                print_help();
+                exit(0);
+            case 'V':
+                cout << versionline << endl;
+                exit(0);
+            default:
+                print_error(argv[0], (char)c);
+                exit(0);
+        }
+    }
+    
+    istream* pios = NULL;
+    ostream* poos = NULL;
+    ifstream* fstr = NULL;
+    ofstream* ofstr = NULL;
+    
+    if (fileset == true) {
+        fstr = new ifstream(seqf);
+        pios = fstr;
+    } else {
+        pios = &cin;
+    }
+    if (outfileset == true) {
+        ofstr = new ofstream(outf);
+        poos = ofstr;
+    } else {
+        poos = &cout;
+    }
+    
+    
+    
+    if (fileset) {
+        fstr->close();
+        delete pios;
+    }
+    if (outfileset) {
+        ofstr->close();
+        delete poos;
+    }
+    return EXIT_SUCCESS;
+}
+
