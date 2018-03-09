959b43e8f15a9d57730cb53bd26ce1c2335787d5
Stephen Smith <blackrim@gmail.com>
2018-03-09 11:31:45 -0500

    added note abut rooting

--- src/main_rmt.cpp
+++ src/main_rmt.cpp
@@ -31,6 +31,8 @@ void print_help() {
     cout << " -h, --help          display this help and exit" << endl;
     cout << " -V, --version       display version and exit" << endl;
     cout << endl;
+    cout << "NOTE: if you get a segfault, you may try unrooting (pxrr -u) before pruning" << endl;
+    cout << endl;
     cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" <<endl;
     cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>"<<endl;
 }
