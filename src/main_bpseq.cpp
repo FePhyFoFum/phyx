07ba289cf321c6e55bd911aadea7f4120bbe0958
Joseph Brown <phylo.jwb@gmail.com>
2019-11-15 14:20:00 -0500

    purge namespace std from pxbpsq

--- src/main_bpseq.cpp
+++ src/main_bpseq.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,8 +7,6 @@
 #include <algorithm>
 #include <set>
 
-using namespace std;
-
 #include "tree.h"
 #include "tree_reader.h"
 #include "sequence.h"
@@ -18,24 +15,25 @@ using namespace std;
 #include "log.h"
 
 void print_help() {
-    cout << "This will print out partitions found in seqfile." << endl;
-    cout << "Can read from stdin or file." << endl;
-    cout << endl;
-    cout << "Usage: pxbpsq [OPTION]... [FILE]..." << endl;
-    cout << endl;
-    cout << " -s, --seqf=FILE     input sequence file, stdin otherwise" << endl;
-    cout << " -t, --treef=FILE    input tree file, stdin otherwise" << endl;
-    cout << " -o, --outf=FILE     output file, stout otherwise" << endl;
-    cout << " -h, --help          display this help and exit" << endl;
-    cout << " -V, --version       display version and exit" << endl;
-    cout << endl;
-    cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" << endl;
-    cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>" << endl;
+    std::cout << "This will print out partitions found in seqfile." << std::endl;
+    std::cout << "Currently only supports DNA alignments. << std::endl";
+    std::cout << "Can read from stdin or file." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Usage: pxbpsq [OPTION]... [FILE]..." << std::endl;
+    std::cout << std::endl;
+    std::cout << " -s, --seqf=FILE     input sequence file, stdin otherwise" << std::endl;
+    std::cout << " -t, --treef=FILE    input tree file, stdin otherwise" << std::endl;
+    std::cout << " -o, --outf=FILE     output file, stout otherwise" << std::endl;
+    std::cout << " -h, --help          display this help and exit" << std::endl;
+    std::cout << " -V, --version       display version and exit" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Report bugs to: <https://github.com/FePhyFoFum/phyx/issues>" << std::endl;
+    std::cout << "phyx home page: <https://github.com/FePhyFoFum/phyx>" << std::endl;
 }
 /*
  * add you name if you contribute (probably add another line)
  */
-string versionline("pxbpsq 0.1\nCopyright (C) 2014 FePhyFoFum\nLicense GPLv3\nwritten by Stephen A. Smith (blackrim)");
+std::string versionline("pxbpsq 0.1\nCopyright (C) 2014 FePhyFoFum\nLicense GPLv3\nwritten by Stephen A. Smith (blackrim), Joseph W. Brown");
 
 static struct option const long_options[] =
 {
@@ -84,7 +82,7 @@ int main(int argc, char * argv[]) {
                 print_help();
                 exit(0);
             case 'V':
-                cout << versionline << endl;
+                std::cout << versionline << std::endl;
                 exit(0);
             default:
                 print_error(argv[0], (char)c);
@@ -92,50 +90,50 @@ int main(int argc, char * argv[]) {
         }
     }
     
-    istream * pios = NULL;
-    istream * piost = NULL;
-    ostream * poos = NULL;
-    ifstream * fstr = NULL;
-    ofstream * ofstr = NULL;
-    ifstream * tfstr = NULL;
+    std::istream * pios = NULL;
+    std::istream * piost = NULL;
+    std::ostream * poos = NULL;
+    std::ifstream * fstr = NULL;
+    std::ofstream * ofstr = NULL;
+    std::ifstream * tfstr = NULL;
     
     if (fileset == true) {
-        fstr = new ifstream(seqf);
+        fstr = new std::ifstream(seqf);
         pios = fstr;
     } else {
-        pios = &cin;
+        pios = &std::cin;
         if (check_for_input_to_stream() == false) {
             print_help();
             exit(1);
         }
     }
     if (outfileset == true) {
-        ofstr = new ofstream(outf);
+        ofstr = new std::ofstream(outf);
         poos = ofstr;
     } else {
-        poos = &cout;
+        poos = &std::cout;
     }
     // if treefile there, read the trees
     // then we will calculate the conditional biparts as well
-    vector<Tree *> trees;
+    std::vector<Tree *> trees;
     if (tfileset == true) {
-        tfstr = new ifstream(treef);
+        tfstr = new std::ifstream(treef);
         piost = tfstr;
         TreeReader tr;
-        string retstring;
+        std::string retstring;
         while (getline(*piost,retstring)) {
             trees.push_back(tr.readTree(retstring));
         }
         int numtrees = trees.size();    
         if (numtrees == 0) {
-            cout << "there are no trees;" << endl;
+            std::cout << "there are no trees;" << std::endl;
         }
     }
 
     // read the sequences
-    vector<Sequence> seqs;
+    std::vector<Sequence> seqs;
     Sequence seq;
-    string retstring;
+    std::string retstring;
     
     int ft = test_seq_filetype_stream(*pios,retstring);
     while (read_next_seq_from_stream(*pios,ft,retstring,seq)) {
@@ -147,13 +145,13 @@ int main(int argc, char * argv[]) {
     }
     
     // get the biparts for the trees
-    vector<string> names;
-    map<string,int> name_index;
-    map<int,string> name_st_index;
+    std::vector<std::string> names;
+    std::map<std::string, int> name_index;
+    std::map<int, std::string> name_st_index;
     for (unsigned int i=0; i < seqs.size(); i++) {
-        string tname = seqs[i].get_id();
+        std::string tname = seqs[i].get_id();
         name_index[tname] = i;
-        (*poos) << tname << " " << i << endl;
+        (*poos) << tname << " " << i << std::endl;
         names.push_back(tname);
         name_st_index[i] = tname;
     }
@@ -162,15 +160,15 @@ int main(int argc, char * argv[]) {
 
     // hardcoded for DNA
     // can try to get more elegant later
-    vector<vector<vector<int> > > all_bp; 
-    vector<double> bp_count;
-    vector<int> seq_bipart_map;
+    std::vector<std::vector<std::vector<int> > > all_bp; 
+    std::vector<double> bp_count;
+    std::vector<int> seq_bipart_map;
     // for each column
     for (int i=0; i < numcols; i++) {
-        vector<int> tbpa;
-        vector<int> tbpc;
-        vector<int> tbpg;
-        vector<int> tbpt;
+        std::vector<int> tbpa;
+        std::vector<int> tbpc;
+        std::vector<int> tbpg;
+        std::vector<int> tbpt;
         int skip = 0;
         for (int j=0; j < numseqs; j++) {
             if (seqs[j].get_sequence()[i] == 'A') {
@@ -182,7 +180,7 @@ int main(int argc, char * argv[]) {
             } else if (seqs[j].get_sequence()[i] == 'T') {
                 tbpt.push_back(j);
             } else {
-                cout << "don't recognize character " << seqs[j].get_sequence()[i] << endl;
+                std::cout << "don't recognize character " << seqs[j].get_sequence()[i] << std::endl;
                 skip += 1;
             }
         }
@@ -193,7 +191,7 @@ int main(int argc, char * argv[]) {
         sort(tbpc.begin(),tbpc.end());
         sort(tbpg.begin(),tbpg.end());
         sort(tbpt.begin(),tbpt.end());
-        vector<vector<int> > tallbp;
+        std::vector<std::vector<int> > tallbp;
         tallbp.push_back(tbpa);
         tallbp.push_back(tbpc);
         tallbp.push_back(tbpg);
@@ -224,19 +222,19 @@ int main(int argc, char * argv[]) {
         }
     }
     
-    (*poos) << numseqs << " sequences with " << numcols << " cols" <<   endl;
-    (*poos) << all_bp.size() << " unique parts found" << endl;
+    (*poos) << numseqs << " sequences with " << numcols << " cols" <<   std::endl;
+    (*poos) << all_bp.size() << " unique parts found" << std::endl;
     
     // calculate the ICA
     // get the parts that are not compatible
     // this would be an intersection of each of the parts and if one is null then it is compatible
     double ACA = 0;
-    vector<double> bp_ica(bp_count.size());
+    std::vector<double> bp_ica(bp_count.size());
     for (unsigned int i=0; i < all_bp.size(); i++) {
         double totalcount = bp_count[i];
-        vector<double> conflict_nums;
+        std::vector<double> conflict_nums;
         conflict_nums.push_back(bp_count[i]);
-        vector<int> conflicts;
+        std::vector<int> conflicts;
         for (unsigned int j=0; j < all_bp.size(); j++) {
             if (i == j) {
                 continue;
@@ -257,7 +255,7 @@ int main(int argc, char * argv[]) {
                     }
                     compcount2 += 1;
                     // intersection of [i][m] and [j][n]
-                    vector<int> v3;
+                    std::vector<int> v3;
                     set_intersection(all_bp[i][m].begin(),all_bp[i][m].end(),all_bp[j][n].begin(),all_bp[j][n].end(),back_inserter(v3));
                     if (v3.size() > 0 && v3.size() < all_bp[j][n].size() && v3.size() < all_bp[i][m].size()) {
                         badcount += 1;
@@ -272,13 +270,13 @@ int main(int argc, char * argv[]) {
                 conflicts.push_back(j);
                 conflict_nums.push_back(bp_count[j]);
                 totalcount += bp_count[j];
-    //        cout << i << " ("<<bp_count[i] << ") and " << j << " ("  <<bp_count[j] << " )" << endl;
-    //        cout << "\t" << get_string_vector(all_bp[i][0]) << " | " << get_string_vector(all_bp[i][1]) << " | " << get_string_vector(all_bp[i][2]) << " | " << get_string_vector(all_bp[i][3]) << endl;
-    //        cout << "\t" << get_string_vector(all_bp[j][0]) << " | " << get_string_vector(all_bp[j][1]) << " | " << get_string_vector(all_bp[j][2]) << " | " << get_string_vector(all_bp[j][3]) << endl;
+    //        std::cout << i << " ("<<bp_count[i] << ") and " << j << " ("  <<bp_count[j] << " )" << std::endl;
+    //        std::cout << "\t" << get_string_vector(all_bp[i][0]) << " | " << get_string_vector(all_bp[i][1]) << " | " << get_string_vector(all_bp[i][2]) << " | " << get_string_vector(all_bp[i][3]) << std::endl;
+    //        std::cout << "\t" << get_string_vector(all_bp[j][0]) << " | " << get_string_vector(all_bp[j][1]) << " | " << get_string_vector(all_bp[j][2]) << " | " << get_string_vector(all_bp[j][3]) << std::endl;
             }
         }
         if (conflict_nums.size() == 1) {
-            (*poos) << i  << " ("<<bp_count[i] << ") no conflict " << get_string_vector(all_bp[i][0]) << " | " <<  get_string_vector(all_bp[i][1]) << " | " << get_string_vector(all_bp[i][2]) << " | " << get_string_vector(all_bp[i][3]) << endl;
+            (*poos) << i  << " ("<<bp_count[i] << ") no conflict " << get_string_vector(all_bp[i][0]) << " | " <<  get_string_vector(all_bp[i][1]) << " | " << get_string_vector(all_bp[i][2]) << " | " << get_string_vector(all_bp[i][3]) << std::endl;
             bp_ica[i] = 1;
             continue;
         }
@@ -297,61 +295,61 @@ int main(int argc, char * argv[]) {
         ACA += ICA;
         ICA *= sign;
         bp_ica[i] = ICA;
-        (*poos) << i <<" ("  << bp_count[i] << ")\t" << ICA << " " << get_string_vector(all_bp[i][0]) << " | " <<  get_string_vector(all_bp[i][1]) << " | " << get_string_vector(all_bp[i][2]) << " | " << get_string_vector(all_bp[i][3]) << endl;
+        (*poos) << i <<" ("  << bp_count[i] << ")\t" << ICA << " " << get_string_vector(all_bp[i][0]) << " | " <<  get_string_vector(all_bp[i][1]) << " | " << get_string_vector(all_bp[i][2]) << " | " << get_string_vector(all_bp[i][3]) << std::endl;
     }
-    (*poos) << " " << ACA << endl;
+    (*poos) << " " << ACA << std::endl;
  
-    //cout << get_string_vector(seq_bipart_map) << endl;
+    //std::cout << get_string_vector(seq_bipart_map) << std::endl;
 
     // tree processing
     if (tfileset) {
         for (unsigned int t=0; t < trees.size(); t++) {
-            vector<string> rt_nms = trees[t]->getRoot()->get_leave_names();
-            set<string> rt_nms_set;
+            std::vector<std::string> rt_nms = trees[t]->getRoot()->get_leave_names();
+            std::set<std::string> rt_nms_set;
             copy(rt_nms.begin(),rt_nms.end(),inserter(rt_nms_set,rt_nms_set.begin()));
             for (int j=0; j < trees[t]->getInternalNodeCount(); j++) {
-                vector<string> nms = trees[t]->getInternalNode(j)->get_leave_names();
-                (*poos) << get_string_vector(nms) << endl;
-                vector<int> nms_i;
-                set<string> nms_s;
+                std::vector<std::string> nms = trees[t]->getInternalNode(j)->get_leave_names();
+                (*poos) << get_string_vector(nms) << std::endl;
+                std::vector<int> nms_i;
+                std::set<std::string> nms_s;
                 copy(nms.begin(),nms.end(),inserter(nms_s,nms_s.begin()));
                 for (unsigned int k=0; k < nms.size(); k++) {
                     nms_i.push_back(name_index[nms[k]]);
                 }
                 sort(nms_i.begin(),nms_i.end());
                 // get the other side of the bipart
-                vector<int> nms_i2;
-                vector<string> nms_s2(rt_nms.size());
-                vector<string>::iterator it;
+                std::vector<int> nms_i2;
+                std::vector<std::string> nms_s2(rt_nms.size());
+                std::vector<std::string>::iterator it;
                 it = set_difference(rt_nms_set.begin(),rt_nms_set.end(),nms_s.begin(),nms_s.end(),nms_s2.begin());
                 nms_s2.resize(it-nms_s2.begin());
                 for (unsigned int k=0; k < nms_s2.size(); k++) {
                     nms_i2.push_back(name_index[nms_s2[k]]);
                 }
                 // find the biparts that are the same
-                vector<int> matches;
+                std::vector<int> matches;
                 for (unsigned int i=0; i < all_bp.size(); i++) {
                     for (unsigned int m=0; m < all_bp[i].size(); m++) {
                         if (all_bp[i][m].size() <= 1 || (all_bp[i][m].size() != nms_i.size() && all_bp[i][m].size() != nms_i2.size() ) ) {
                             continue;
                         }
-                        vector<int> v3;
-                        set_intersection(all_bp[i][m].begin(),all_bp[i][m].end(),nms_i.begin(),nms_i.end(),back_inserter(v3));
+                        std::vector<int> v3;
+                        std::set_intersection(all_bp[i][m].begin(),all_bp[i][m].end(),nms_i.begin(),nms_i.end(),back_inserter(v3));
                         if (v3.size() == nms_i.size()) {
                             (*poos) << i << " (" << bp_ica[i] << ") ";
                             break;
                         }
-                        vector<int> v4;
-                        set_intersection(all_bp[i][m].begin(),all_bp[i][m].end(),nms_i2.begin(),nms_i2.end(),back_inserter(v4));
+                        std::vector<int> v4;
+                        std::set_intersection(all_bp[i][m].begin(),all_bp[i][m].end(),nms_i2.begin(),nms_i2.end(),back_inserter(v4));
                         if (v4.size() == nms_i2.size()) {
                             (*poos) << i << " (" << bp_ica[i] << ") ";
                             break;
                         }
-                    }//each part of the bipart
-                }//each bipart
-                (*poos) << endl;
-            }//each internal node
-        }//each tree
+                    } //each part of the bipart
+                } //each bipart
+                (*poos) << std::endl;
+            } //each internal node
+        } //each tree
         tfstr->close();
         delete piost;
     }
