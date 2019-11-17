1866c61112b554ca106946294f7dc8bc7d691e2e
Joseph Brown <phylo.jwb@gmail.com>
2019-11-17 10:54:58 -0500

    another orphan

--- src/orphan_files/main_test.cpp
+++ src/orphan_files/main_test.cpp
@@ -11,10 +11,6 @@
 
 // g++ -std=c++11 main_test.cpp -o JWB_test
 
-//g++ -std=c++11 nj.cpp main_nj.cpp utils.cpp superdouble.cpp sequence.cpp seq_reader.cpp seq_utils.cpp log.cpp -o test
-
-using namespace std;
-
 #include "vcf_reader.h"
 
 /*
@@ -37,24 +33,24 @@ int main(int argc, char * argv[]){
     TreeReader tr;
 
     if (argc != 1){
-         std::cout << "usage: phyx_test" << std::endl;
+        std::cout << "usage: phyx_test" << std::endl;
         exit(0);
     }
     string datafile = "../../../projects/PHLAWD_fish/12S.keep";
     vector<Sequence> seqs;
     FastaUtil pr;
     bool phyl = pr.readFile(datafile,seqs);
-     std::cout << "sequences: " << seqs.size() << std::endl;
-     std::cout << seqs[0].get_sequence() <<std::endl;
+    std::cout << "sequences: " << seqs.size() << std::endl;
+    std::cout << seqs[0].get_sequence() <<std::endl;
     seqs[0].reverse_complement();
-     std::cout << std::endl;
-     std::cout << seqs[0].get_sequence()<< std::endl;
-     std::cout << "writing file" << std::endl;
+    std::cout << std::endl;
+    std::cout << seqs[0].get_sequence()<< std::endl;
+    std::cout << "writing file" << std::endl;
     string outfile = "test.fasta";
     pr.writeFileFromVector(outfile,seqs);
     */
     
-     std::cout << "werked" << std::endl;
+    std::cout << "werked" << std::endl;
     
     return EXIT_SUCCESS;
 }
