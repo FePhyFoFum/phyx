7ac5eb9ec8eac925cc89ea0541bca579d963c1df
Joseph Brown <phylo.jwb@gmail.com>
2021-04-19 19:38:13 -0400

    ┬─┬ノ( º _ ºノ)

--- src/constants.h
+++ src/constants.h
@@ -9,6 +9,6 @@ const long double PI = 3.1415926535897932384626433832795L;
 const long double E = 2.718281828459045;
 
 // Phyx citation
-std::string PHYX_CITATION = "Brown, Joseph W., Joseph F. Walker, and Stephen A. Smith. 2017. Phyx: phylogenetic tools for unix. Bioinformatics, 33(12), 1886-1888.";
+std::string PHYX_CITATION = "Brown, Joseph W., Joseph F. Walker, and Stephen A. Smith. 2017. Phyx: phylogenetic tools for unix. Bioinformatics 33(12): 1886-1888. https://doi.org/10.1093/bioinformatics/btx063";
 
 #endif /* _CONSTANTS_H_ */
