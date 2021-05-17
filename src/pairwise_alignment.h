be02d1db0dad53b23e47f74bf7284dffdf7fefee
Joseph Brown <phylo.jwb@gmail.com>
2021-05-17 11:52:16 -0400

    clang-tidy: consistent arg names

--- src/pairwise_alignment.h
+++ src/pairwise_alignment.h
@@ -6,9 +6,9 @@
 
 class Sequence; // forward declaration
 
-double nw(Sequence& seq1, Sequence& seq2, std::map<char, std::map<char, int> >& sc_mat,
+double nw(Sequence& iseq1, Sequence& iseq2, std::map<char, std::map<char, int> >& scoringmatrix,
     double gap_penalty, std::string& aln1, std::string& aln2);
-double sw(Sequence& seq1, Sequence& seq2, std::map<char, std::map<char, int> >& sc_mat,
+double sw(Sequence& iseq1, Sequence& iseq2, std::map<char, std::map<char, int> >& scoringmatrix,
     double gap_penalty, std::string& aln1, std::string& aln2);
 
 #endif /* _PAIRWISE_ALIGNMENT_H_ */
